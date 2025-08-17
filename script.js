48f0618b1b7179a910051297f931179523fdb2ae
Monatric <nawpnawp8@gmail.com>
2025-08-17 19:58:20 +0800

    feat: add Book constructor and func to add books to library

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("js connected")
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  if (!new.target) {
+    throw Error("You must use 'new' to call the constructor")
+  }
+
+  this.id = crypto.randomUUID();
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.isRead = isRead
+
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus()}`
+  }
+
+  this.readStatus = function () {
+    return this.isRead ? "read already" : "not read yet"
+  }
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  const book = new Book(title, author, pages, isRead)
+  myLibrary.push(book)
+}
+
+const theHobbit = addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true)
+console.log(myLibrary)
